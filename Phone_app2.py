f00c7926bf254b9515967938997b90ec5c59875b
CTBrumley <toddbrumley30@gmail.com>
2019-02-04 21:12:16 -0500

    Add files via upload

---
+++ Phone_app2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  8 06:17:52 2019
+
+@author: todd
+"""
+
+class Phone:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+        self.phones = []
+    def getPrice(self):
+        return self.name
+    def getName(self):
+        return self.name
+    
+    
+
+    
+    
+        
+    
